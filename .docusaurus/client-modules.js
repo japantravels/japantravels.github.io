3e0c26b1af7b276c892fe77f0c018286fda79760
Nagy Sándor <nagysa1313@gmail.com>
2023-09-07 10:18:39 +0300

    Initial add of docusaurus

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/sandornagy/Documents/GitHub/japantravels.github.io/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/sandornagy/Documents/GitHub/japantravels.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/sandornagy/Documents/GitHub/japantravels.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/sandornagy/Documents/GitHub/japantravels.github.io/src/css/custom.css'),
+];
